77bcb4deae9edffc6c29391013b1f59592a3c0c4
Philippe Recto <precto1285@gmal.com>
2018-03-15 18:16:50 -0700

    Adjusted about page

---
+++ assets/javascript/script.js
@@ -0,0 +1,10 @@
+ // Initialize Firebase
+ var config = {
+    apiKey: "AIzaSyBPDLKrGgOhWJT7MGerzTClc4Rr-fFghqo",
+    authDomain: "alethean-guitar.firebaseapp.com",
+    databaseURL: "https://alethean-guitar.firebaseio.com",
+    projectId: "alethean-guitar",
+    storageBucket: "alethean-guitar.appspot.com",
+    messagingSenderId: "514506135156"
+  };
+  firebase.initializeApp(config);
